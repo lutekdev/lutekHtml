51667f629dd113ac22c14c6fb7db366c15c1838d
LutekDev <lutekdev@gmail.com>
2024-03-06 21:55:59 -0300

    🐛 FIX: Update Folders and Files in Date 06/03/24

---
+++ HTML/html.AULAS/HCode.Aulas/aula.001/index.js
@@ -0,0 +1,15 @@
+let nome = "Traz"
+
+console.log(`Bem vindo ${nome}!`)
+
+/**
+ *  Criando Mensagens de Boas Vindas
+ */
+
+function boasVindas() {
+    console.log(`Bem vindo ${nome}!`);
+    console.log(`Seja muito bem vindo(a) ${nome}!`);
+    console.log(`Bem vindo ${nome}!`);
+}
+
+boasVindas()
